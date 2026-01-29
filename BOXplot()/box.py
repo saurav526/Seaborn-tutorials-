9e2c054155c53aab883814d9f4bc9772c4753928
saurav526 <sauravtiwari2712005@gmail.com>
2026-01-29 08:38:29 +0530

    box plot

---
+++ BOXplot()/box.py
@@ -0,0 +1,31 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips = sns.load_dataset("tips")
+sns.set_style("whitegrid")
+sns.boxplot(
+    data=tips, 
+    x="day", 
+    y="total_bill", 
+    hue="smoker", 
+    order=["Thur", "Fri", "Sat", "Sun"],
+    saturation=1.0,
+    width=0.6,
+    hue_order=["No", "Yes"],
+    fliersize=5,
+    linewidth=3,
+    orient="v",
+    gap=0.1,
+    palette="plasma",
+    meanprops={"marker":"D", "markerfacecolor":"red", "markeredgecolor":"black", "markersize":8},
+    showmeans=True,
+    flierprops={"marker":"o", "markerfacecolor":"green", "markeredgecolor":"black", "markersize":5},
+    showfliers=True,
+    
+    
+    )  
+
+plt.title("Box Plot of Total Bill by Day and Smoker Status")
+plt.legend(title="Legend", bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+plt.grid(True , linestyle='--', linewidth=0.5, color='gray', alpha=0.7)
+plt.show()
